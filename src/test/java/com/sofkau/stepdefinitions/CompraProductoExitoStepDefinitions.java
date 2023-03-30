0eaddc1ac81ffc767afae970ada009112ecc283a
Nevardo Antonio Ospina Zuñiga <ospina_88@hotmail.com>
2023-03-30 10:41:14 -0500

    Se crea el featue y la clase CompraProducto

---
+++ src/test/java/com/sofkau/stepdefinitions/CompraProductoExitoStepDefinitions.java
@@ -0,0 +1,31 @@
+package com.sofkau.stepdefinitions;
+
+import com.sofkau.setup.Configuracion;
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+
+public class CompraProductoExitoStepDefinitions extends Configuracion {
+
+
+    @Dado("que el usuario esta en la pagina de inicio del exito")
+    public void que_el_usuario_esta_en_la_pagina_de_inicio_del_exito() {
+
+    }
+
+
+
+
+    @Cuando("completa los campos para iniciar sesion")
+    public void completa_los_campos_para_iniciar_sesion() {
+
+    }
+
+
+    @Entonces("el usuario debe ver un mensaje de hola cuando ingrese a su cuenta")
+    public void el_usuario_debe_ver_un_mensaje_de_hola_cuando_ingrese_a_su_cuenta() {
+
+    }
+}
